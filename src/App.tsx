f388017def5a8abfb546d9ee4d2353e1a722e6aa
NaruseNia <notshinsin0000@gmail.com>
2024-07-28 16:20:43 +0900

    Setup solid-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { ColorModeProvider, ColorModeScript, createLocalStorageManager } from "@kobalte/core";
+import { Route, Router } from "@solidjs/router";
+import { Suspense } from "solid-js";
+import { Root } from "~/pages/Root";
+
 export const App = () => {
-  return <div></div>;
+  const storageManager = createLocalStorageManager("vite-ui-theme");
+  return (
+    <Router root={(props) => (
+      <>
+        <ColorModeScript storageType={storageManager.type} />
+        <ColorModeProvider storageManager={storageManager}>
+          <Suspense fallback={null}>{props.children}</Suspense>
+        </ColorModeProvider>
+      </>
+    )}>
+      <Route path="/" component={Root} />
+    </Router>
+  );
 };
