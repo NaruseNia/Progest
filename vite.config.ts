5338b9c0ccc2bc6dfd368bfa7de5a5223d7fe9ee
NaruseNia <notshinsin0000@gmail.com>
2024-07-28 15:58:33 +0900

    Setup and refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import electron from 'vite-plugin-electron';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import electron from "vite-plugin-electron";
+import solidPlugin from "vite-plugin-solid";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [
@@ -8,16 +9,21 @@ export default defineConfig({
     electron([
       {
         // Main-Process entry file of the Electron App.
-        entry: 'electron/main.ts',
+        entry: "electron/main.ts",
       },
       {
-        entry: 'electron/preload.ts',
+        entry: "electron/preload.ts",
         onstart(options) {
           // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
           // instead of restarting the entire Electron App.
-          options.reload()
+          options.reload();
         },
       },
     ]),
   ],
-})
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    }
+  }
+});
